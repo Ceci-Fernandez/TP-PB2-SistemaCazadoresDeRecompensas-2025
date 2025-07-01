e4579eb79cbafb0b553f816ab5dc7b69ffcb19d0
Nahuel0911 <nahuelesposito01@gmail.com>
2025-07-01 20:54:40 -0300

    Actualizacion Cazador
    Movi todo al mismo package
    Cree los 3 hijos de Cazador
    Cazador paso a ser clase abstracta
    Los 3 hijos sobreescriben la funcionalidad puedeCapturar cumpliendo con las 3 partes de la consigna (Afectados por el entorno en caso de CazadorRural y CazadorUrbano puse que le sume + 20 de habilidad)
    Borre el enum TipoCazador

---
+++ src/ar/edu/unlam/pb2/cazadores/CazadorUrbano.java
@@ -0,0 +1,22 @@
+package ar.edu.unlam.pb2.cazadores;
+
+import ar.edu.unlam.pb2.tipos.Entorno;
+
+public class CazadorUrbano extends Cazador {
+    public CazadorUrbano(String nombre, int experiencia) {
+        super(nombre, experiencia);
+    }
+
+    @Override
+    public boolean puedeCapturar(Profugo profugo, Entorno entorno) {
+    	int expActual = this.getExperiencia();
+    	
+    	if(entorno == entorno.URBANO) {
+    		expActual += 20;
+    	}
+    	this.intimidar(profugo);
+        return expActual > profugo.getNivelInocencia() && profugo.isNervioso() != true ;
+    }
+
+
+}
