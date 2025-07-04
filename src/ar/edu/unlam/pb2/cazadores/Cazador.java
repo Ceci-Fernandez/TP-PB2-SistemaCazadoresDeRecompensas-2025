8b376d94eaf3e289725b578868b404114d3681ab
Nahuel0911 <nahuelesposito01@gmail.com>
2025-07-04 11:50:17 -0300

    Cazador + Cazador Rural + Profugo (Algunos cambios)
    Hice algunos cambios a profugos (Protegi los atributos evolucionables)
    Cazador lo mismo -> Lo adapte para que sea mas legible, intimidar especifico ahora estara en las clases hijo e igualmente habra un intimidar en Cazador. Cambie un poco el toString
    Cazador Sigiloso -> Cambie lo de intimidacion

--- src/ar/edu/unlam/pb2/cazadores/Cazador.java
+++ src/ar/edu/unlam/pb2/cazadores/Cazador.java
@@ -1,59 +1,89 @@
 package ar.edu.unlam.pb2.cazadores;
 
+import java.util.*;
+
 import ar.edu.unlam.pb2.profugo.Profugo;
 import ar.edu.unlam.pb2.sistema.Zona;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public abstract class Cazador {
     protected String nombre;
-    protected int experiencia;
-    protected List<Profugo> capturados;
+    protected Integer experiencia;
+    protected List<Profugo> profugosCapturados;
+    
 
-    public Cazador(String nombre, int experiencia) {
+    public Cazador(String nombre, Integer experienciaInicial) {
         this.nombre = nombre;
-        this.experiencia = experiencia;
-        this.capturados = new ArrayList<>();
+        this.experiencia = experienciaInicial;
+        this.profugosCapturados = new ArrayList<>();
     }
-
-    public void registrarCaptura(Profugo progufo) {
-        capturados.add(progufo);
-    }
-
-    public List<Profugo> getCapturados() {
-        return capturados;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getExperiencia() {
-        return experiencia;
-    }
-
-    public void intimidarProfugo(Profugo profugo) {
+    
+    public void realizarCaptura(Zona zona) {
+        List<Profugo> profugosIntimidados = new ArrayList<>();
+        Set<Profugo> profugosARemover = new HashSet<>();
+        
+        int capturasAntes = profugosCapturados.size();      
        
+        for (Profugo profugo : zona.getProfugos()) {
+            if (puedeCapturar(profugo)) {
+                capturarProfugo(profugo);
+                profugosARemover.add(profugo);
+            } else {
+                intimidarProfugo(profugo);
+                profugosIntimidados.add(profugo);
+            }
+        }
+        
+        zona.removerProfugos(profugosARemover);
+        
+
+        int nuevasCapturas = profugosCapturados.size() - capturasAntes;
+        sumarExperiencia(profugosIntimidados, nuevasCapturas);
+    }
+    
+    private Boolean puedeCapturar(Profugo profugo) {
+        Boolean condicionGeneral = this.experiencia > profugo.getNivelInocencia();
+        Boolean condicionEspecifica = capturaEspecifica(profugo);
+        return condicionGeneral && condicionEspecifica;
+    }
+    
+    protected abstract Boolean capturaEspecifica(Profugo profugo);
+    
+    protected abstract void intimidacionEspecifica(Profugo profugo);
+    
+    private void capturarProfugo(Profugo profugo) {
+        profugosCapturados.add(profugo);
     }
 
-	public boolean puedeCapturar(Profugo profugo) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public int getCantidadCapturas() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public void realizarCaptura(Zona zona) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public List<Profugo> getProfugosCapturados() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private void intimidarProfugo(Profugo profugo) {
+        profugo.reducirInocencia(2); 
+        intimidacionEspecifica(profugo); 
+    }
+    
+    private void sumarExperiencia(List<Profugo> intimidados, int nuevasCapturasEnEsteRound) {
+        int minimoHabilidadIntimidados = 0;
+        
+        if (!intimidados.isEmpty()) {
+            minimoHabilidadIntimidados = intimidados.get(0).getNivelHabilidad();
+            for (Profugo profugo : intimidados) {
+                if (profugo.getNivelHabilidad() < minimoHabilidadIntimidados) {
+                    minimoHabilidadIntimidados = profugo.getNivelHabilidad();
+                }
+            }
+        }
+        
+        int experienciaGanada = minimoHabilidadIntimidados + (2 * nuevasCapturasEnEsteRound);
+        this.experiencia += experienciaGanada;
+    }
+ 
+    public String getNombre() { return nombre; }
+    public int getExperiencia() { return experiencia; }
+    public List<Profugo> getProfugosCapturados() { return new ArrayList<>(profugosCapturados); }
+    public int getCantidadCapturas() { return profugosCapturados.size(); }
+    
+    @Override
+    public String toString() {
+        return String.format("%s[Experiencia: %d, Capturas: %d]", 
+                           getClass().getSimpleName(), experiencia, profugosCapturados.size());
+    }
 }
